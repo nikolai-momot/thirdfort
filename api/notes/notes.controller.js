7410072af7aaeaccc7b43acc0d7daf4271bdad9a
Nikolai Momot <nmomot@mapador.com>
2022-06-17 13:22:08 +0200

    Fix Header Issue
    Forgot that headers are lowercase only, change header from userId to
    x-user-id

--- api/notes/notes.controller.js
+++ api/notes/notes.controller.js
@@ -4,6 +4,7 @@ const service = require('./notes.service');
 
 exports.getOne = async ctx => {
   const { noteId } = ctx.params;
+  const userId = ctx.request.headers['x-user-id'];
   const note = await service.getOne({ ...ctx.params, userId })
   ctx.assert(note, 404, `Note with ID ${noteId} doesn't exist`);
   ctx.status = 200;
@@ -12,7 +13,7 @@ exports.getOne = async ctx => {
 
 exports.updateOne = async ctx => {
   const { noteId } = ctx.params;
-  const { userId } = ctx.request.headers;
+  const userId = ctx.request.headers['x-user-id'];
   const note = await service.updateOne({ ...ctx.params, userId }, ctx.request.body)
   ctx.assert(note, 404, `Note with ID ${noteId} could not be updated`);
   ctx.status = 200;
@@ -20,14 +21,15 @@ exports.updateOne = async ctx => {
 };
 
 exports.getAll = async ctx => {
-  const { userId } = ctx.request.headers;
+  const userId = ctx.request.headers['x-user-id'];
+  console.log('ctx.request.headers', ctx.request.headers)
   const notes = await service.getAll({ ...ctx.request.query, userId })
   ctx.status = 200;
   ctx.body = notes;
 };
 
 exports.createOne = async ctx => {
-  const { userId } = ctx.request.headers;
+  const userId = ctx.request.headers['x-user-id'];
   const note = await service.createOne({ userId }, ctx.request.body)
   ctx.assert(note, 400, 'The note info is malformed!');
   ctx.status = 201;
--- api/notes/notes.repository.js
+++ api/notes/notes.repository.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const db = require('../../knexfile.js');
+const config = require('../../knexfile.js');
+const knex = require('knex')
+const db = knex(config['production']) // Replace with NODE_ENV after stability
 
 const columns = [
     'id',
@@ -10,17 +12,17 @@ const columns = [
 ]
 
 exports.getOne = async ({ userId, noteId }) => {
-    return db('notes').where({ id: noteId, user_id: userId }).first(columns);
+    return await db('notes').where({ id: noteId, user_id: userId }).first(columns);
 };
 
 exports.getAll = async ({ userId, archived }) => {
-    return db('notes').where({ user_id: userId, archived }).select(columns);
+    return await db('notes').where({ user_id: userId, archived: !!archived }).select(columns);
 };
 
 exports.createOne = async ({ userId }, body) => {
-    return db('notes').insert({ ...body, user_id: userId })
+    return await db('notes').insert({ ...body, user_id: userId })
 };
 
 exports.updateOne = async ({ userId, noteId }, body) => {
-    return db('notes').where({ id: noteId, user_id: userId }).update(columns, { ...body, id: noteId, user_id: userId });
+    return await db('notes').where({ id: noteId, user_id: userId }).update(columns, { ...body, id: noteId, user_id: userId });
 };
